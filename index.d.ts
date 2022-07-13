da52b7e8f2d37856a7162f622c0cab8bfb72a4b4
ModyQyW <wurui-dev@foxmail.com>
2022-07-13 15:59:18 +0800

    feat: update progress types

--- index.d.ts
+++ index.d.ts
@@ -663,7 +663,8 @@ type TMovableView = TComponent<
      */
     scaleMax: number;
     /**
-     * @desc 定义缩放倍数，取值范围为 0.5 - 10
+     * @desc 定义缩放倍数
+     * @desc 取值范围为 0.5 - 10
      * @desc 默认为 1
      */
     scaleValue: number;
@@ -847,25 +848,68 @@ type TRichText = TComponent<
   {}
 >;
 
-/** 进度条 */
+/**
+ * @desc 进度条
+ */
 type TProgress = TComponent<
   {
-    /** 百分比0~100 */
+    /**
+     * @desc 百分比
+     * @desc 取值范围为 0 - 100
+     * @desc 没有默认值
+     */
     percent: number;
-    /** 在进度条右侧显示百分比 */
+    /**
+     * @desc 是否在进度条右侧显示百分比
+     * @desc 默认为 false
+     */
     showInfo: boolean;
-    /** 进度条线的宽度，单位px */
+    /**
+     * @desc 圆角大小
+     * @desc 默认为 0
+     */
+    borderRadius: number | string;
+    /**
+     * @desc 进度条右侧显示的百分比字体大小
+     * @desc 默认为 16
+     */
+    fontSize: number | string;
+    /**
+     * @desc 进度条线的宽度
+     * @desc 单位为 px
+     * @desc 默认为 6
+     */
     strokeWidth: number;
-    /** switch 的颜色，同 css 的 color */
-    color: string;
-    /** radio已选择的颜色 */
+    /**
+     * @desc 已选择的进度条的颜色
+     * @desc 默认为 #09bb07，百度默认为 #e6e6e6
+     */
     activeColor: string;
-    /** radio背景条的颜色 */
+    /**
+     * @desc 未选择的进度条的颜色
+     * @desc 默认为 #ebebeb
+     */
     backgroundColor: string;
-    /** 进度条从左往右的动画 */
+    /**
+     * @desc 是否显示进度条从左往右的动画
+     * @desc 默认为 false
+     */
     active: boolean;
-    /** backwards: 动画从头播；forwards：动画从上次结束点接着播 */
-    activeMode: string;
+    /**
+     * @desc 动画播放方式
+     * @desc 默认为 backwards
+     */
+    activeMode: "backwards" | "forwards";
+    /**
+     * @desc 进度增加 1% 所需时间
+     * @desc 单位为 ms
+     * @desc 默认为 30
+     */
+    duration: number;
+    /**
+     * @desc 动画完成时触发
+     */
+    onActiveend: (event: TBaseEvent) => void;
   },
   {}
 >;
@@ -1633,6 +1677,7 @@ type TUnicloudDb = TComponent<
 declare module "@vue/runtime-core" {
   export interface GlobalComponents {
     Block: TBlock;
+
     View: TView;
     ScrollView: TScrollView;
     Swiper: TSwiper;
@@ -1642,11 +1687,12 @@ declare module "@vue/runtime-core" {
     MovableView: TMovableView;
     CoverView: TCoverView;
     CoverImage: TCoverImage;
+
     Icon: TIcon;
     Text: TText;
     RichText: TRichText;
-
     Progress: TProgress;
+
     Button: TButton;
     CheckboxGroup: TCheckboxGroup;
     Checkbox: TCheckbox;
