1fb43af0baa68cdf9a7f18c9669c0ed5ae456310
ModyQyW <wurui-dev@foxmail.com>
2022-07-13 10:53:51 +0800

    feat: TBlock

--- index.d.ts
+++ index.d.ts
@@ -28,7 +28,11 @@ type TComponent<
   Readonly<ExtractPropTypes<P>>
 >;
 
-/** block节点 */
+/**
+ * @desc 包装元素，不会在页面中做任何渲染，只接受控制属性
+ * @desc 支持在 template 模板中嵌套 template 和 block
+ * @desc 在不同的平台表现存在一定差异，推荐统一使用 template
+ */
 type TBlock = TComponent<{}, {}>;
 
 /** 基本视图容器 */
