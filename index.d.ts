643e96b981723428cb018ddc6fc5fdf060d7307a
ModyQyW <wurui-dev@foxmail.com>
2022-07-13 17:20:01 +0800

    feat: update form types

--- index.d.ts
+++ index.d.ts
@@ -1215,13 +1215,45 @@ type TEditor = TComponent<
   {}
 >;
 
-/** 表单 */
+/**
+ * @desc form submit 事件对象
+ */
+type TFormSubmitEvent = TEvent & {
+  detail: {
+    value: {
+      [key: string]: number | string | boolean;
+    };
+    formId?: string;
+  };
+};
+/**
+ * @desc 表单
+ * @desc 将组件内的用户输入的 switch、input、checkbox、slider、radio、picker 提交
+ */
 type TForm = TComponent<
   {
-    /** 是否禁用。 */
-    disabled: boolean;
-    /** 是否返回 formId 用于发送模板消息 */
+    /**
+     * @desc 是否返回 formId 用于发送模板消息
+     * @desc 默认为 false
+     */
     reportSubmit: boolean;
+    /**
+     * @desc 等待一段时间以确认 formId 是否生效
+     * @desc 如果未指定这个参数，formId 有很小的概率无效（网络问题）
+     * @desc 指定这个参数将可以检测 formId 是否有效，以这个参数的时间作为这项检测的超时时间
+     * @desc 如果无效，将返回 requestFormId:fail 开头的 formId
+     * @desc 单位为 ms
+     * @desc 默认为 0
+     */
+    reportSubmitTimeout: number;
+    /**
+     * @desc 表单提交时触发
+     */
+    onSubmit: (event: TFormSubmitEvent) => void;
+    /**
+     * @desc 表单重置时触发
+     */
+    onReset: (event: TEvent) => void;
   },
   {}
 >;
