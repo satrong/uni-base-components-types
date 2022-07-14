e4008ed0ef64f949e986b8ceabda7b57076fc3b3
ModyQyW <wurui-dev@foxmail.com>
2022-07-14 13:44:26 +0800

    feat: update slider types

--- index.d.ts
+++ index.d.ts
@@ -1869,33 +1869,90 @@ type TRadio = TComponent<
   {}
 >;
 
-/** 滑动选择器 */
+/**
+ * @desc slider change 事件对象
+ */
+type TSliderChangeEvent = TEvent & {
+  detail: {
+    value: number;
+  };
+};
+/**
+ * @desc slider changing 事件对象
+ */
+type TSliderChangingEvent = TEvent & {
+  detail: {
+    value: number;
+  };
+};
+/**
+ * @desc 滑动选择器
+ */
 type TSlider = TComponent<
   {
-    /** 是否禁用。 */
-    disabled: boolean;
-    /**  radio 最小值 */
+    /**
+     * @desc 在 form 中作为 key
+     */
+    name: string;
+    /**
+     * @desc 最小值
+     * @desc 默认为 0
+     */
     min: number;
-    /** radio最大值 */
+    /**
+     * @desc 最大值
+     * @desc 默认为 100
+     */
     max: number;
-    /** radio步长，取值必须大于 0，并且可被(max - min)整除 */
+    /**
+     * @desc 步长，取值必须大于 0，并且可被 (max - min) 整除
+     * @desc 默认为 1
+     */
     step: number;
-    /** radio当前取值 */
+    /**
+     * @desc 是否禁用
+     * @desc 默认为 false
+     */
+    disabled: boolean;
+    /**
+     * @desc 当前取值
+     * @desc 默认为 0
+     */
     value: number;
-    /** switch 的颜色，同 css 的 color */
-    color: string;
-    /** radio已选择的颜色 */
-    selectColor: string;
-    /** radio已选择的颜色 */
+    /**
+     * @desc 滑块左侧已选择部分的线条颜色
+     * @desc 默认为各平台默认色
+     */
     activeColor: string;
-    /** radio背景条的颜色 */
+    /**
+     * @desc 滑块右侧背景条的颜色
+     * @desc 默认为 #e9e9e9
+     */
     backgroundColor: string;
-    /** radio滑块的大小，取值范围为 12 - 28 */
+    /**
+     * @desc 滑块的大小
+     * @desc 取值范围为 12 - 28
+     * @desc 默认为 28
+     */
     blockSize: number;
-    /** 滑块颜色 */
+    /**
+     * @desc 滑块的颜色
+     * @desc 默认为 #fff
+     */
     blockColor: string;
-    /** 是否显示当前 value */
+    /**
+     * @desc 是否显示当前 value
+     * @desc 默认为 false
+     */
     showValue: boolean;
+    /**
+     * @desc 完成一次拖动后触发
+     */
+    onChange: (event: TSliderChangeEvent) => void;
+    /**
+     * @desc 拖动过程中触发
+     */
+    onChanging: (event: TSliderChangingEvent) => void;
   },
   {}
 >;
