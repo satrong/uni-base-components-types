c7530ef88baeb52ab19ac21a94782ff70067b0e7
ModyQyW <wurui-dev@foxmail.com>
2022-07-14 17:24:01 +0800

    feat: update ad types

--- index.d.ts
+++ index.d.ts
@@ -3574,33 +3574,92 @@ type TWebView = TComponent<
   {}
 >;
 
-/** 用于展示微信开放的数据 */
-type TOpenData = TComponent<
-  {
-    /** 开放数据类型 */
-    type: string;
-    /** 当 type="groupName" 时生效, 群id */
-    openGid: string;
-    /**  */
-    lang: "ts";
-  },
-  {}
->;
-
-/** 广告 */
+/**
+ * @desc ad error 事件对象
+ */
+type TAdErrorEvent = TEvent & {
+  detail: {
+    errCode: number;
+  };
+};
+/**
+ * @desc 广告
+ */
 type TAd = TComponent<
   {
-    /** App广告位id，在uni-AD官网申请广告位 */
+    /**
+     * @desc APP 广告位 id */
     adpid: string;
-  },
-  {}
->;
-
-/** Draw 信息流广告 */
-type TAdDraw = TComponent<
-  {
-    /** App广告位id，在uni-AD官网申请广告位 */
-    adpid: string;
+    /**
+     * @desc 广告单元 id，可在小程序管理后台的流量主模块新建
+     */
+    unitId: string;
+    /**
+     * @desc 广告自动刷新的间隔时间，必须大于等于 30
+     * @desc 该参数不传入时 Banner 广告不会自动刷新
+     * @desc 单位为 s
+     */
+    adIntervals: number;
+    /**
+     * @desc 广告数据，优先级高于 adpid
+     */
+    data: Record<string, any>;
+    /**
+     * @desc 小程序应用 ID
+     */
+    appid: string;
+    /**
+     * @desc 小程序广告位 ID
+     */
+    apid: string;
+    /**
+     * @desc type 为 feeds 时广告左边距，必须大于 0
+     * @desc 单位为 px
+     */
+    adLeft: number;
+    /**
+     * @desc type 为 feeds 时广告上边距，必须大于 0
+     * @desc 单位为 px
+     */
+    adTop: number;
+    /**
+     * @desc type 为 feeds 时广告宽度，最大值为屏幕宽度，最小值为 265
+     * @desc 单位为 px
+     * @desc 默认为 100%
+     */
+    adWidth: number;
+    /**
+     * @desc type 为 feeds 时广告高度，最大值为 160，最小值为 85
+     * @desc 单位为 px
+     */
+    adHeight: number;
+    /**
+     * @desc 广告类型
+     */
+    type:
+      | "banner"
+      | "card"
+      | "feeds"
+      | "block"
+      | "feed"
+      | "video"
+      | "large"
+      | "llmg"
+      | "rlmg"
+      | "swip"
+      | "grid";
+    /**
+     * @desc 广告加载成功的回调
+     */
+    onLoad: (event: TEvent) => void;
+    /**
+     * @desc 广告加载失败的回调
+     */
+    onError: (event: TAdErrorEvent) => void;
+    /**
+     * @desc 广告关闭的回调
+     */
+    onClose: (event: TEvent) => void;
   },
   {}
 >;
@@ -3723,6 +3782,19 @@ type TUnicloudDb = TComponent<
   {}
 >;
 
+/** 用于展示微信开放的数据 */
+type TOpenData = TComponent<
+  {
+    /** 开放数据类型 */
+    type: string;
+    /** 当 type="groupName" 时生效, 群id */
+    openGid: string;
+    /**  */
+    lang: "ts";
+  },
+  {}
+>;
+
 declare module "@vue/runtime-core" {
   export interface GlobalComponents {
     Block: TBlock;
@@ -3774,7 +3846,6 @@ declare module "@vue/runtime-core" {
     WebView: TWebView;
 
     Ad: TAd;
-    AdDraw: TAdDraw;
 
     UnicloudDb: TUnicloudDb;
 
