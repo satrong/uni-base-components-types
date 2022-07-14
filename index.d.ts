5391d77fa83a6f33c048bc7a5bbb60192af65b5c
ModyQyW <wurui-dev@foxmail.com>
2022-07-14 17:00:27 +0800

    feat: update web-view types

--- index.d.ts
+++ index.d.ts
@@ -3523,13 +3523,53 @@ type TCanvas = TComponent<
   {}
 >;
 
-/** 承载网页的容器 */
+/**
+ * @desc web-view message 事件对象
+ */
+type TWebViewMessageEvent = TEvent & {
+  detail: {
+    data: any[];
+  };
+};
+/**
+ * @desc web 浏览器组件，可承载网页
+ */
 type TWebView = TComponent<
   {
-    /** webview 指向网页的链接 */
+    /**
+     * @desc webview 指向网页的链接
+     */
     src: string | string;
-    /** webview 的样式 */
-    webviewStyles: Record<string, any> | boolean;
+    /**
+     * @desc 用于为 iframe 指定其特征策略
+     */
+    allow: string;
+    /**
+     * @desc 该属性对呈现在 iframe 框架中的内容启用一些额外的限制条件
+     */
+    sandbox: string;
+    /**
+     * @desc webview 的样式
+     */
+    webviewStyles: {
+      progress:
+        | {
+            color: string;
+          }
+        | boolean;
+    };
+    /**
+     * @desc 是否自动更新当前页面标题
+     */
+    updateTitle: boolean;
+    /**
+     * @desc 网页向应用 postMessage 时，会在特定时机（后退、组件销毁、分享）触发并收到消息
+     */
+    onMessage: (event: TWebViewMessageEvent) => void;
+    /**
+     * @desc 网页向应用实时 postMessage
+     */
+    onPostMessage: (event: TEvent) => void;
   },
   {}
 >;
